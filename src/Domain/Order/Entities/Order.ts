99b13d96dc9ca90c645a2ade697e2ba38702ec8e
bladibap <bladimirbaptistagonzales@gmail.com>
2026-04-08 21:02:20 -0400

    test precommit

--- src/Domain/Order/Entities/Order.ts
+++ src/Domain/Order/Entities/Order.ts
@@ -24,6 +24,7 @@ export class Order extends AggregateRoot {
 		if (this.status === StatusOrder.COMPLETED){
 			throw new DomainException( OrderError.canNotChangeStatus(this.status, StatusOrder.COMPLETED) );
 		}
+
 		if (!this.verifyIfAllItemsCompleted()) {
 			throw new DomainException( OrderError.orderItemsNotCompleted(this.id) );
 		}
